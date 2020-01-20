3f173c5ddcc8ce1eb01e062b4a6c22fcb6ff5e8a
Chiadmy <hamza.chyadmi@gmail.com>
2020-01-20 03:01:07 +0100

    welcome page

--- app/src/main/java/com/example/location_busdeteceting/MainActivity.java
+++ app/src/main/java/com/example/location_busdeteceting/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.location_busdeteceting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView bgapp, clover;
+    LinearLayout textsplash, texthome, menus;
+    Animation frombottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frombottom = AnimationUtils.loadAnimation(this, R.anim.frombottom);
+
+
+        bgapp = (ImageView) findViewById(R.id.bgapp);
+        clover = (ImageView) findViewById(R.id.clover);
+        textsplash = (LinearLayout) findViewById(R.id.textsplash);
+        texthome = (LinearLayout) findViewById(R.id.texthome);
+        menus = (LinearLayout) findViewById(R.id.menus);
+
+        bgapp.animate().translationY(-2000).setDuration(2000).setStartDelay(300);
+        clover.animate().translationX(-2000).alpha(0).setDuration(3000).setStartDelay(600);
+        textsplash.animate().translationY(140).alpha(0).setDuration(800).setStartDelay(300);
+        texthome.startAnimation(frombottom);
+        menus.startAnimation(frombottom);
+
+
     }
 }
